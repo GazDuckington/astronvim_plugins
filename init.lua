3c187ee2c03ecd8831bd3339d78a0afca285bf0b
Ghazy <dianghazy@protonmail.com>
2023-04-13 09:07:08 +0700

    first commit

---
+++ init.lua
@@ -0,0 +1,8 @@
+-- community plugins
+return {
+  plugins = {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.motion.nvim-surround" },
+    { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  }
+}
